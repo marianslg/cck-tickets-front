45cc682d556d9680272dcac8a5d4cd8143cd2310
Mariano <salguero.ms@gmail.com>
2022-07-13 23:18:04 -0300

    .

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 var ul = document.getElementById("event-list");
 
-fetch('https://cck-tickets.herokuapp.com/getAllEvents', {
-        'mode': 'no-cors',
-        'headers': {
-            'Access-Control-Allow-Origin': '*',
-        }
-    })
+fetch('https://cck-tickets.herokuapp.com/getAllEvents')
     .then(response => response.json())
     .then(data => {
 
