2d0f01cfd87f7e1da74837fd6f13b95e369994ca
Mariano <salguero.ms@gmail.com>
2022-07-13 23:20:04 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-var ul = document.getElementById("event-list");
-
 fetch('https://cck-tickets.herokuapp.com/getAllEvents')
     .then(response => response.json())
     .then(data => {
-
+        var ul = document.getElementById("event-list");
         data.forEach(element => {
             var li = document.createElement("li");
             li.appendChild(document.createTextNode(element));
