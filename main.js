e96a492c93b3f7259cb9cdd23d9e1daaab4fb262
Mariano <salguero.ms@gmail.com>
2022-07-22 16:45:08 -0300

    .

--- main.js
+++ main.js
@@ -14,10 +14,11 @@ fetch('https://cck-tickets.herokuapp.com/getAllEvents')
 
             var h5 = document.createElement("h5");
             h5.className = 'mb-1 title'
-            h5.innerText = (element.data.soldOut !== undefined && element.data.soldOut == true ? "(Agotado) " : "") + element.data.name
+            console.log(element.soldOut)
+            h5.innerText = (element.soldOut != 'undefined' && element.soldOut == 1 ? "(Agotado) " : "") + element.name
 
             var small = document.createElement("small");
-            small.innerText = element.data.date
+            small.innerText = element.eventDate
             small.className = "date"
 
             var p = document.createElement("p");
@@ -29,13 +30,13 @@ fetch('https://cck-tickets.herokuapp.com/getAllEvents')
             a.appendChild(div)
             a.appendChild(p)
 
-            if (element.data.reserva != undefined) {
+            if (element.reserve != undefined) {
                 var small2 = document.createElement("small");
-                small2.innerText = element.data.reserva
+                small2.innerText = element.reserve
                 a.appendChild(small2)
             }
 
-            if (element.data.soldOut == true)
+            if (element.soldOut !== undefined && element.soldOut == true)
                 a.style.backgroundColor = 'rgba(255, 182, 193, .3)';
 
             div1.appendChild(a);
